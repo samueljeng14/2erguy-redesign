aa9e6f4de05267e84c20c07c4031896049cd4026
Samuel Jeng <samueljengmts@gmail.com>
2019-04-02 00:19:36 +0800

    upload

---
+++ index.js
@@ -0,0 +1,18 @@
+new TypeIt('#typed', {
+    strings: ["私房景點", "觀光導覽", "美食夜市", "戶外活動"],
+    speed: 200,
+    deleteSpeed: 50,
+    startDelay: 1,
+    breakLines: false,
+    autoStart: false,
+    loop: true,
+    loopDelay: 0,
+    nextStringDelay: [600, 2400],
+    lifeLike: false
+});
+
+// $(document).ready(function() {
+//     $(".go-button").click(function() {
+//         $("html, body").animate({scrollTop: 4000}, 20000)
+//     });
+// });
